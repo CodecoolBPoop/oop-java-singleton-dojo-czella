1f36749f7a5cf20b4946e28d2efaec9f622ebec4
Laszlo Czellaho <laszlo.czellaho@gmail.com>
2019-01-22 09:58:37 +0100

    Add solution for both questions

--- src/main/java/com/codecool/singletonDojo/Printer.java
+++ src/main/java/com/codecool/singletonDojo/Printer.java
@@ -1,14 +1,20 @@
 package com.codecool.singletonDojo;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Printer {
 
     private int id; // ID of the printer. Unique.
     private LocalTime busyEndTime;
+    private static List<Printer> instances = new ArrayList<>();
+    private static int count;
 
-    public Printer(int id) {
+    private Printer(int id) {
         this.id = id;
+        this.busyEndTime = LocalTime.now();
     }
 
     // Prints out the given String
@@ -18,8 +24,32 @@ public class Printer {
         busyEndTime = LocalTime.now().plusSeconds(5);
     }
 
+
     // Returns true if the printer is ready to print now.
     public boolean isAvailable() {
         return LocalTime.now().isAfter(busyEndTime);
+    }
+
+    public static Printer getInstance() {
+        if (instances.size() == 0) {
+            for (int i = 0; i < 10; i++) {
+                count++;
+                instances.add(new Printer(count));
+            }
+        }
+        List<Printer> availablePrinters = new ArrayList<>();
+        for (Printer printer: instances) {
+            if (printer.isAvailable()) {
+                availablePrinters.add(printer);
+            }
+        }
+        if (availablePrinters.size() > 0) {
+            return availablePrinters.get(ThreadLocalRandom.current().nextInt(0, availablePrinters.size()));
+        } else {
+            System.out.println("Everybody busy");
+            return instances.get(ThreadLocalRandom.current().nextInt(0, instances.size()));
+        }
+
+
     }
 }
